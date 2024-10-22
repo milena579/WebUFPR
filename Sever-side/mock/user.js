97a9973b14d793ed1e2e827accca4e03e02fae8a
Milena <100636013+milena579@users.noreply.github.com>
2024-10-21 21:34:38 -0300

    Create user.js

---
+++ Sever-side/mock/user.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        id : 1,
+        nome: "Milena",
+        idade : 19
+    },
+    {
+        id : 2,
+        nome: "Ingrid",
+        idade : 19
+    },
+    {
+        id : 1,
+        nome: "Amanda",
+        idade : 18
+    },
+]
