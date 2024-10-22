aa1524c09a5943a8936dcae764aa288828a0baeb
Milena <100636013+milena579@users.noreply.github.com>
2024-10-21 21:35:01 -0300

    Create comida.js

---
+++ Sever-side/mock/comida.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        nome: "chocolate"
+    },
+    {
+        id: 2,
+        nome: "beijinho"
+    },
+    {
+        id: 3,
+        nome: "dois-amores"
+    },
+]
